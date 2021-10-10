4c2dc6248451e7f7bcb0079db9651cfadc53532d
figoysra <figoyusra.133@gmail.com>
2021-10-10 19:47:15 +0700

    adding socket function and messagemodel

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyparser = require('body-parser')
 const cors = require('cors')
 const http = require('http')
 const { Server } = require('socket.io')
+const Messages = require('./src/models/messagesModel')
+const {Op} = require("sequelize")
 
 const app  = express()
 app.use(cors())
@@ -18,6 +20,38 @@ const io = new Server(httpServer, {
         origin: "*",
     },
 });
+
+io.on("connection", (socket)=>{
+    console.log("a User Connected")
+    socket.on("login", (room)=>{
+        console.log("a User joined room" + room)
+        socket.join(room)
+    })
+    socket.on("send-message", async(payload)=>{
+        const {sender, receiver, msg} = payload
+        const sendMessage = await Messages.create({
+            sender : sender,
+            receiver : receiver,
+            msg : msg
+        })
+        io.to(receiver).emit("list-message", payload);
+    })
+    socket.on("get-message",async(payload)=>{
+        const {sender, receiver} = payload
+        const getMessage = await Messages.findAll({
+            where: {
+                [Op.or] : [{
+                    sender : sender,
+                    receiver: receiver
+                },{
+                    sender: receiver,
+                    receiver: sender
+                }]
+            },
+        })
+        io.to(sender).emit("history-messages", getMessage);
+    })
+})
 
 const PORT = 2021
 httpServer.listen(PORT, () => {
